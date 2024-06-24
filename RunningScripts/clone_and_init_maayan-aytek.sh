ef7a74f021814f655911b7a24b5dd9a079c40bff
maayan-aytek <maayan.aytek@campus.technion.ac.il>
2024-06-24 15:49:35 +0300

    submission files

---
+++ RunningScripts/clone_and_init_maayan-aytek.sh
@@ -0,0 +1,13 @@
+# cloning your code from github:
+git clone https://github.com/maayan-aytek/HumanChoicePredictionNLP.git
+
+# Your main sweep:
+python final_sweep_YOUR_UID.py
+
+# More runs appear in your report:
+python sweep1_baseline.py
+python sweep2_random_EDA.py
+python sweep3_rf_model.py
+python sweep4_heuristic_tuning.py
+python sweep5_heuristic_tuning2.py
+python sweep6_heuristic_bins_rep.py
