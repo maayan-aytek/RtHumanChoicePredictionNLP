f8ca692c6b4c52d263d6c91206dfc3b58228d97e
maayan-aytek <81248290+maayan-aytek@users.noreply.github.com>
2024-08-14 21:39:38 +0300

    Update clone_and_init_maayan-aytek.sh

--- RunningScripts/clone_and_init_maayan-aytek.sh
+++ RunningScripts/clone_and_init_maayan-aytek.sh
@@ -1,15 +1,17 @@
 # cloning your code from github:
 git clone https://github.com/maayan-aytek/RtHumanChoicePredictionNLP.git
 
-# Your main sweep:
+# Our final (best) sweep:
 python final_sweep_maayan-aytek.py
 
-# More runs appear in your report:
+# Older sweeps (not included in the paper)
 python sweep1_baseline.py
 python sweep2_random_EDA.py
 python sweep3_rf_model.py
 python sweep4_heuristic_tuning.py
 python sweep5_heuristic_tuning2.py
+
+# Sweeps reported in the paper
 python sweep6_random_bins_rep.py
 python sweep7_rf_model_bins_rep.py
 python sweep8_heuristic_bins_rep.py
